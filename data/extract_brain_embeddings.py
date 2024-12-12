593bf08a83cdeff48d0d0a7589663c2d31988394
dwemer8 <kornilov.dv.work@gmail.com>
2024-12-12 13:49:03 +0000

    Modifying extract_brain_embeddings.py for the sake of BraTS_2023 support

--- data/extract_brain_embeddings.py
+++ data/extract_brain_embeddings.py
@@ -13,25 +13,32 @@ import os
 data = pd.read_csv("./data/files/dataframe_brain.csv")
 encoder = MRIEncoder(2, 512, False)
 encoder.load_state_dict(
-    clean_state_dict(torch.load("data/models/t1ce-flair_tumorTrue.pth")), strict=False
+    # clean_state_dict(torch.load("data/models/t1ce-flair_tumorTrue.pth")), strict=False
+    clean_state_dict(torch.load("data/models/t1c-t2f_tumorTrue.pth")), strict=False
 )
 encoder.eval()
 for mri_path in tqdm.tqdm(data.MRI):
-    if pd.isna(mri_path):
-        continue
-    process = MRIProcessor(
-        mri_path,
-        tumor_centered=True,
-        transform=tumor_transfo,
-        modalities=["t1ce", "flair"],
-        size=(64, 64, 64),
-    )
-    mri = process.process().unsqueeze(0)
-    with torch.no_grad():
-        embedding = encoder(mri)
+    try:
+        if pd.isna(mri_path):
+            continue
+        process = MRIProcessor(
+            mri_path,
+            tumor_centered=True,
+            transform=tumor_transfo,
+            # modalities=["t1ce", "flair"],
+            modalities=["t1c", "t2f"],
+            size=(64, 64, 64),
+        )
+        mri = process.process().unsqueeze(0)
+        with torch.no_grad():
+            embedding = encoder(mri)
 
-    # Save the embedding
-    with open(os.path.join(mri_path, "embedding.csv"), "w") as f:
-        writer = csv.writer(f)
-        writer.writerow([str(i) for i in range(512)])
-        writer.writerow(embedding.squeeze().numpy().tolist())
+        # Save the embedding
+        with open(os.path.join(mri_path, "embedding.csv"), "w") as f:
+            writer = csv.writer(f)
+            writer.writerow([str(i) for i in range(512)])
+            writer.writerow(embedding.squeeze().numpy().tolist())
+    except KeyboardInterrupt:
+        break
+    except Exception as e:
+        print(f"ERROR for {mri_path}: {e}")
